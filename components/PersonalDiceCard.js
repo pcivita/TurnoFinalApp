ecd4bccc88af822f9558759f0d2e170922cd4305
malinac02 <malinacalarco@gmail.com>
2024-02-22 23:04:29 -0800

    tabs

--- components/PersonalDiceCard.js
+++ components/PersonalDiceCard.js
@@ -8,67 +8,67 @@ const windowHeight = Dimensions.get('window').height;
 export default function PersonalDiceCard({ img, title, user, numSaved, numRolled}) {
   return (
     <View style={styles.container}>
-        <Image source={img} style={styles.image}/>
-        <View style={styles.row1}>
-            <FontAwesome5 name="dice-d6" size={16} color="black" />
-            <Text style={styles.titleText}>{title}</Text>    
-        </View>
-        {/* <View style={{flexDirection: 'row', alignItems: 'center', marginVertical: 2, marginLeft: 10}}>
-            <Image source={user.profilePic} style={styles.profilePic}/>
-            <Text style={{fontSize: 12}}>By @{user.username}</Text>
-        </View>
-        <View style={{flexDirection: 'row', alignItems: 'center', marginLeft: 10, marginTop: 8}}>
-            <Image style={styles.bookmarkIcon} source={require('../assets/Themes/Images/other/bookmarkGrey.png')}/>
-            <Text style={styles.statsText}>{numSaved}</Text>
-            <Image style={styles.bookmarkIcon} source={require('../assets/Themes/Images/other/diceRoll.png')}/>
-            <Text style={styles.statsText}>{numRolled}</Text>
-        </View> */}
+      <Image source={img} style={styles.image}/>
+      <View style={styles.row1}>
+        <FontAwesome5 name="dice-d6" size={16} color="black" />
+        <Text style={styles.titleText}>{title}</Text>    
+      </View>
+      {/* <View style={{flexDirection: 'row', alignItems: 'center', marginVertical: 2, marginLeft: 10}}>
+        <Image source={user.profilePic} style={styles.profilePic}/>
+        <Text style={{fontSize: 12}}>By @{user.username}</Text>
+      </View>
+      <View style={{flexDirection: 'row', alignItems: 'center', marginLeft: 10, marginTop: 8}}>
+        <Image style={styles.bookmarkIcon} source={require('../assets/Themes/Images/other/bookmarkGrey.png')}/>
+        <Text style={styles.statsText}>{numSaved}</Text>
+        <Image style={styles.bookmarkIcon} source={require('../assets/Themes/Images/other/diceRoll.png')}/>
+        <Text style={styles.statsText}>{numRolled}</Text>
+      </View> */}
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: (windowWidth / 2) - (windowWidth * 0.05),
-        borderRadius: 20,
-        height: 150,
-        borderWidth: 1,
-        borderColor: Themes.colors.mediumGray,
-    },
-    row1: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-        gap: 5,
-        flex: 1,
-    },
-    image: {
-        width: "100%",
-        height: 100,
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-    },
-    titleText: {
-        fontFamily: 'Poppins-Regular',
-        fontSize: 14,
-        // marginTop: 10,
-        // marginLeft: 10,
-    },
-    profilePic: {
-        width: 30,
-        height: 30,
-        borderRadius: 999,
-        marginRight: 10,
-    }, 
-    statsText: {
-        fontFamily: 'Poppins-Regular',
-        fontSize: 12,
-        marginRight: 4,
-        color: "#9C9C9C",
-    },
-    bookmarkIcon: {
-        width: 16,
-        height: 16,
-        marginRight: 2,
-    }
+  container: {
+    width: (windowWidth / 2) - (windowWidth * 0.05),
+    borderRadius: 20,
+    height: 150,
+    borderWidth: 1,
+    borderColor: Themes.colors.mediumGray,
+  },
+  row1: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 5,
+    flex: 1,
+  },
+  image: {
+    width: "100%",
+    height: 100,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  titleText: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 14,
+    // marginTop: 10,
+    // marginLeft: 10,
+  },
+  profilePic: {
+    width: 30,
+    height: 30,
+    borderRadius: 999,
+    marginRight: 10,
+  }, 
+  statsText: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 12,
+    marginRight: 4,
+    color: "#9C9C9C",
+  },
+  bookmarkIcon: {
+    width: 16,
+    height: 16,
+    marginRight: 2,
+  }
 })
