90c7673252ce5ed3cf96fbc243084af83452f84a
Caleb Liu <cjliu2003@gmail.com>
2024-03-09 10:13:58 -0800

    supabase connected

--- Supabase.js
+++ Supabase.js
@@ -5,12 +5,11 @@ import { createClient } from "@supabase/supabase-js";
 // const supabaseUrl = "https://onguxyfmoofxaytmfhqs.supabase.co";
 // const supabaseAnonKey =
 //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uZ3V4eWZtb29meGF5dG1maHFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE5NzgzODQsImV4cCI6MjAxNzU1NDM4NH0.ZMWv8U4MMD2zq-d6PavO6PAkgr6WYzSpvw2_dqEvBrc";
-
-const projectUrl = "https://mkdaoilcthudkfmkkhuo.supabase.co"
-const newAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rZGFvaWxjdGh1ZGtmbWtraHVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY3NDE4NDEsImV4cCI6MjAyMjMxNzg0MX0.l58T8TLUZ6_T-969M-BiCBxosdIBzhDA-swiSSRTTnc"
+const SUPABASE_URL= "https://mkdaoilcthudkfmkkhuo.supabase.co"
+const SUPABASE_ANON_KEY= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rZGFvaWxjdGh1ZGtmbWtraHVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY3NDE4NDEsImV4cCI6MjAyMjMxNzg0MX0.l58T8TLUZ6_T-969M-BiCBxosdIBzhDA-swiSSRTTnc"
 const newSecretKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rZGFvaWxjdGh1ZGtmbWtraHVvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNjc0MTg0MSwiZXhwIjoyMDIyMzE3ODQxfQ.JgC1k-IoEwMCHXhMPY3gXAKpxqJX5XIyct8uZJRQVhk'
 
-export default supabase = createClient(projectUrl, newAnonKey, {
+export default supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
