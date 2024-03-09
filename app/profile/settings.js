19cddf5d5e784a8beb6ef4903e9ceb559fe48ec1
malinac02 <malinacalarco@gmail.com>
2024-03-08 22:40:20 -0800

    edit settings page

--- app/profile/settings.js
+++ app/profile/settings.js
@@ -1,7 +1,12 @@
-import { StyleSheet, Text, View, TextInput,
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
   Keyboard,
   TouchableOpacity,
-  Dimensions, } from "react-native";
+  Dimensions,
+} from "react-native";
 import { Link, Stack, router } from "expo-router";
 import { Themes } from "../../assets/Themes";
 import Category from "../../components/Category";
@@ -9,91 +14,75 @@ import Header from "../../components/Header";
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../contexts/UserContext";
 
-
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 export default function Page() {
-  const {logoutUser, user, fetchUserFromUid} = useContext(UserContext);
+  const { logoutUser, user, fetchUserFromUid } = useContext(UserContext);
 
   const [uid, setUid] = useState(null);
   useEffect(() => {
     if (user) {
       setUid(user.uid);
     }
-  }, [user])
+  }, [user]);
 
   const [userData, setUserData] = useState({});
   useEffect(() => {
     const fetchUserData = async () => {
       let result = await fetchUserFromUid(user.uid);
       setUserData(result);
-    }
+      console.log("HII: ", result);
+    };
     if (user) {
       fetchUserData();
       // console.log(userData.password);
     }
-  }, [user])
+  }, [user]);
 
   const handleLogout = () => {
     router.replace("/");
     logoutUser();
-  }
-  
+  };
+
   return (
     <View style={styles.container}>
-      <Stack.Screen
-        options={{headerShown: false }}
-      />
+      <Stack.Screen options={{ headerShown: false }} />
       <Header title="Settings" />
-      <View style={styles.activityNameContainer}>
-        <Text style={styles.subtitle}>User ID</Text>
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Name</Text>
         <TextInput
           editable={false}
           style={styles.input}
-          value={uid ? uid : ""}
-          // onChangeText={setActivityName}
+          value={userData.fullName}
         />
       </View>
-      <View style={styles.activityNameContainer}>
-        <Text style={styles.subtitle}>Username</Text>
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Email</Text>
+        <TextInput
+          editable={false}
+          style={styles.input}
+          value={userData.email} // userData.email //userData.profilePicUri
+        />
+      </View>
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Username</Text>
         <TextInput
           editable={false}
           style={styles.input}
           value={userData.username}
-          // onChangeText={setActivityName}
         />
       </View>
-      <View style={styles.activityNameContainer}>
-        <Text style={styles.subtitle}>Password</Text>
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Password</Text>
         <TextInput
           editable={false}
           style={styles.input}
           value="*****************"
-          // onChangeText={setActivityName}
         />
       </View>
       <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
         <Text style={styles.logoutText}>Log Out</Text>
       </TouchableOpacity>
-
-      <View style={styles.addToDiceContainer}>
-        <Link
-          // disabled={!isFormFilled}
-          href={{
-            pathname: "/activities/home",
-            params: {
-              name: "Alan",
-            },
-          }}
-          // onPress={handleAddActivity}
-        >
-          <View
-            // style={isFormFilled ? styles.buttonEnabled : styles.buttonDisabled}
-          >
-            <Text style={styles.addToDice}>Add to Dice</Text>
-          </View>
-        </Link>
-      </View>
     </View>
   );
 }
@@ -103,110 +92,47 @@ const styles = StyleSheet.create({
     display: "flex",
     flex: 1,
     gap: 5,
-    backgroundColor: Themes.colors.background
+    backgroundColor: Themes.colors.background,
   },
   logoutButton: {
-    backgroundColor: Themes.colors.salmon,
-    padding: 20,
-    width: windowWidth * 0.6,
+    marginTop: 32,
+    height: 40,
+    width: 360,
     alignItems: "center",
     justifyContent: "center",
-    borderRadius: 5,
-    marginTop: 32,
     alignSelf: "center",
+    borderRadius: 20,
+    backgroundColor: Themes.colors.salmon,
   },
   logoutText: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "white",
-  },
-  titleContainer: {
-    height: "10%",
-    backgroundColor: Themes.colors.salmon,
-    borderColor: "black",
-    justifyContent: "center",
-  },
-  title: {
-    fontWeight: "bold",
-    fontSize: 32,
-    color: "white",
-    alignSelf: "center",
+    fontSize: 16,
     fontFamily: "Poppins-Bold",
+    color: "white",
   },
-  subtitle: {
+  sectionTitle: {
     marginHorizontal: 12,
-    fontSize: 20,
-    fontWeight: "bold",
-    fontFamily: "Poppins-Bold",
+
+    fontSize: 18,
+    fontFamily: "Poppins-SemiBold",
   },
   input: {
-    flex: 1,
-    marginHorizontal: 12,
+    // fontFamily: "Poppins-Regular",
+    // backgroundColor: Themes.colors.lightGray,
+    // borderRadius: 5,
+    // borderWidth: 3,
+    // borderColor: Themes.colors.lightGray,
+    // padding: 10,
     fontSize: 16,
-    fontFamily: "Poppins-Regular",
-    backgroundColor: Themes.colors.lightGray,
+    padding: 10,
+    marginHorizontal: 12,
     borderRadius: 5,
-    borderWidth: 3,
-    borderColor: Themes.colors.lightGray,
-    padding: 10,
+    borderWidth: 1,
+    borderColor: Themes.colors.darkGray,
+    height: 45,
+    fontFamily: "Poppins-Regular",
   },
-  activityNameContainer: {
-    paddingTop: "3%",
-    height: "14%",
-    gap: "10%",
-  },
-  descriptionContainer: {
-    paddingTop: "2%",
-    height: "23%",
-    gap: "10%",
-  },
-  categoriesContainer: {
-    paddingTop: "2%",
-    paddingBottom: 0,
-    height: "37%",
-    gap: "10%",
-  },
-  categories: {
+  section: {
     gap: 10,
-    margin: 12,
-    marginTop: 0,
-    flex: 1,
-    height: "40%",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-between",
-  },
-  addToDiceContainer: {
-    height: "8%",
-    margin: 12,
-    justifyContent: "flex-end",
-    alignItems: "flex-end",
-  },
-  buttonEnabled: {
-    backgroundColor: Themes.colors.salmon,
-    padding: 10,
-    width: "100%",
-    flex: 1,
-    alignContent: "center",
-    justifyContent: "center",
-    borderRadius: 20,
-  },
-  buttonDisabled: {
-    backgroundColor: Themes.colors.salmonTransparent,
-    padding: 10,
-    width: "100%",
-    flex: 1,
-    alignContent: "center",
-    justifyContent: "center",
-    borderRadius: 20,
-  },
-  addToDice: {
-    alignSelf: "center",
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "white",
-  },
-  asterick: {
-    color: Themes.colors.salmon,
+    paddingTop: 10,
   },
 });
